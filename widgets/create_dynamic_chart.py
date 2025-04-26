4ed6943337449a443398602778a92b22ba60f643
Jing Wang <daisydream00@gmail.com>
2025-04-26 12:34:01 +0800

    Update create_dynamic_chart.py

--- widgets/create_dynamic_chart.py
+++ widgets/create_dynamic_chart.py
@@ -54,15 +54,16 @@ with st.container(border=True):
     all_years = st.checkbox("Show prices for the whole period")
     year = st.slider("Select a year", 2005, 2009, disabled=all_years)
  
-    if not all_years:
-        chart_data = source.query(f"date < {year + 1} and date >= {year} and symbol in {stocks}")
-    else:
-        chart_data = source.query(f"symbol in {stocks}")  
-        year = "2005-2009"
+
     if not stocks:
         st.error("Please select at least one stock!")
     else:
-        st.markdown(f"### Stock prices in {year}<br>", unsafe_allow_html=True)
+        if not all_years:
+            st.markdown(f"### Stock prices in {year}")
+            chart_data = source.query(f"date < {year + 1} and date >= {year} and symbol in {stocks}")
+        else:
+            st.markdown(f"### Stock prices in all years")
+            chart_data = source.query(f"symbol in {stocks}")  
         st.line_chart(chart_data, x="date", y="price", color="symbol", 
                       width=720, height=500)
 
