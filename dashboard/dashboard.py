cd1c99bf67fbdb53dc60e8a189d7da1a1634efbb
Jing Wang <daisydream00@gmail.com>
2025-05-05 12:03:08 +0800

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -4,7 +4,26 @@ import pandas as pd
 
 st.set_page_config(page_title="Stock Dashboard", layout="wide")
 st.markdown("""### :material/description:  Requirements
-""")
+- Please download `In-Class Exercise_5_part1.ipynb` from Canvas. Complete the code to (1) scrape the tickers/symbols of S&P 100 companies from the [Wikipedia page](https://en.wikipedia.org/wiki/S%26P_100); 
+and (2) extract relevant info from the **`yfinance`** libarary to generate two files `ticker_info.csv` and `stock_data.csv` (The two data files are also available on Canvas if you want to start with the second part first). Place these two files into your streamlit project folder and complete
+this second part of this exercise, which is to set up a streamlit application.
+            
+- Include a sidebar (`with st.`sidebar`) that contains the following widgets:
+    - A checkbox widget (`st.checkbox()`) that alows the user to control whether to show a bar chart that visualizes the market cap of S&P 100 stocks by sector; the default value is checked.
+    - A multiselect widget (`st.multiselect()`) that allows the user to choose which stocks should be included for plotting; the options are S&P 100 tickers; the default values are `'TSLA'`, `'NVDA'`, and `'AAPL'`.
+    - A slider widget (`st.slider()`) that allows the user to specify the year range over which stocks are compared; the range is 2020 - 2025; the default value is 2004 - 2025.
+
+- If the checkbox is checked, display a bar chart that visualizes the market cap of S&P 100 stocks by sector.
+
+- If none of the stocks are selected, display an error message: "Please select at least one stock!"; 
+  if stocks are selected, show a line chart for closing prices (using the column 'Close') and a bar chart for volume (using the column 'Volume').
+            
+- Tips:
+    - Use the `.unique()` method to obtain the unique values of `Ticker` column in the Pandas dataframe `ticker_info`;
+    - [`DataFrame.query()`](https://pandas.pydata.org/docs/user_guide/indexing.html#the-query-method) can be used to filter data for plotting, 
+        - e.g., if the variable holding the selected stocks is `selected_tickers` and the variable holding the year range is `selected_years`, `query(f"Date < {selected_years[1] + 1} and Date >= {selected_years[0]} and Ticker in {selected_tickers}")` will give you the desired subset of data.
+""")          
+
 
 st.divider()
 
@@ -12,7 +31,6 @@ st.markdown("#### :material/widgets: :red[Expected output]")
 
 st.divider()
 
-st.title("S&P 100 Stock Dashboard 📊")  #:bar_chart:
 
 # load stock info and data
 ticker_info = pd.read_csv("https://raw.githubusercontent.com/daisydream00/tutorial/refs/heads/main/static/ticker_info.csv")
