bc60e1555b5740b419235610e558bc3ea83b6f07
Jing Wang <daisydream00@gmail.com>
2026-04-28 16:30:21 +0800

    Update widget_types.py

--- widgets/widget_types.py
+++ widgets/widget_types.py
@@ -72,7 +72,7 @@ with st.container(border=True):
     with st.echo("below"):
         # default to an empty string
         txt_str = st.text_area("Text to analyze")
-st.markdown(f"The value of variable `name_str` is<br>`'''{txt_str}'''`<br>of `{type(txt_str)}`",
+st.markdown(f"The value of variable `txt_str` is<br>`'''{txt_str}'''`<br>of `{type(txt_str)}`",
             unsafe_allow_html=True)
 
 st.info("Simply typing inside the widget won't rerun the widget function to return a new value. An update is triggered either by clicking or tabbing out of the widget or by pressing `Enter`.",
