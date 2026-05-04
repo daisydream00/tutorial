9f192f579d3c6b3a322b6a3989a8e6feac875d00
Jing Wang <daisydream00@gmail.com>
2026-05-04 17:24:25 +0800

    Create exercise.py

---
+++ exercise/exercise.py
@@ -0,0 +1,100 @@
+import streamlit as st
+import pandas as pd
+
+
+# Configure page
+st.set_page_config(page_title="Stock Dashboard", layout="wide")
+
+st.markdown("""### :material/description:  Requirements
+
+
+- :red[Optional task]: Please download `In-Class Exercise_5_part1.ipynb` from Canvas. Complete the code to extract relevant info from the **`yfinance`** libarary to generate two files `ticker_info.csv` and `stock_data.csv`.
+
+- The two data files are also available on Canvas if you want to skip the first part. Place these two files into your streamlit project folder and complete
+this second part of this exercise, which is to set up a streamlit application.
+            
+- Include a sidebar (`with st.sidebar`) that contains the following widgets:
+    - A checkbox widget (`st.checkbox()`) that alows the user to control whether to show a bar chart that visualizes the market cap of S&P 100 stocks by sector; the default value is checked.
+    - A multiselect widget (`st.multiselect()`) that allows the user to choose which stocks should be included for plotting; the options are S&P 100 tickers; the default values are `'TSLA'`, `'NVDA'`, and `'AAPL'`.
+    - A slider widget (`st.slider()`) that allows the user to specify the year range over which stocks are compared; the range is 2020 - 2026; the default value is 2005 - 2026.
+           
+- If the checkbox is checked, display a bar chart that visualizes the market cap of S&P 100 stocks by sector.
+
+- If none of the stocks are selected, display an error message: "Please select at least one stock!"; 
+  if stocks are selected, show a line chart for closing prices (using the column 'Close') and a bar chart for volume (using the column 'Volume').
+            
+- Tips:
+    - Use the `.unique()` method to obtain the unique values of `Ticker` column in the Pandas dataframe `ticker_info`;
+    - [`DataFrame.query()`](https://pandas.pydata.org/docs/user_guide/indexing.html#the-query-method) can be used to filter data for plotting, 
+        - e.g., if the variable holding the selected stocks is `selected_tickers` and the variable holding the year range is `selected_years`, `query(f"Date < {selected_years[1] + 1} and Date >= {selected_years[0]} and Ticker in {selected_tickers}")` will give you the desired subset of data.
+""")          
+
+
+# load stock info and data
+ticker_info = pd.read_csv("ticker_info.csv")
+stock_data = pd.read_csv("stock_data.csv", parse_dates=['Date'])
+
+# extract S&P 100 tickers from ticker_info
+# fill in the code below
+tickers_100 = ticker_info.Ticker.unique()
+
+# set page title
+st.title("S&P 100 Stock Dashboard 📊")  
+
+# Sidebar controls
+# fill in the code below
+with st.sidebar:
+    st.header("Sidebar Widgets")
+
+    # Checkbox widget
+    # fill in the code below
+    show_sector = st.checkbox("Show Market Cap by Sector", True)
+
+    # Multiselect widget
+    # fill in the code below
+    selected_tickers = st.multiselect(
+        "Select Companies",
+        options=tickers_100,
+        default=['TSLA', 'NVDA', 'AAPL']
+    )
+
+    # Year range slider
+    # fill in the code below
+    selected_years = st.slider(
+        "Select Year Range",
+        min_value=2020,
+        max_value=2026,
+        value=(2025, 2026)
+    )
+
+# a bar chart that visualizes the market cap of S&P 100 stocks by sector
+# fill in the code below
+if show_sector:
+    st.header("Market Cap by Sector")
+    st.bar_chart(ticker_info, x="Sector", y="Market Cap (B)", color="Ticker", horizontal=True,
+            width=720, height=500)
+
+# display price and volme charts if stocks are selected; show error message otherwise
+# fill in the code below
+if selected_tickers:
+    st.header(f"Stock Trend Analysis ({selected_years[0]} - {selected_years[1]})")
+    chart_data = stock_data.query(f"Date < {selected_years[1] + 1} and Date >= {selected_years[0]} and Ticker in {selected_tickers}")
+ 
+    # Line chart for closing price
+    st.subheader("Closing Prices")
+    st.line_chart(chart_data, x="Date", y="Close", color="Ticker",
+                  width=720, height=500)
+
+    
+    # Bar chart for volume
+    st.subheader("Trading Volume")
+    st.bar_chart(chart_data, x="Date", y="Volume", color="Ticker", 
+                  width=720, height=500)
+
+# display the error message
+else:
+    st.error("Please select at least one stock!")
+
+
+
+
